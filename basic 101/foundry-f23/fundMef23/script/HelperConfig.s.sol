2ecac1e36a419137e15a0ac52f4129a894d0b511
panditdhamdhere <dhamdherepandit@gmail.com>
2024-03-21 23:32:14 +0530

    helperconfig

---
+++ basic 101/foundry-f23/fundMef23/script/HelperConfig.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+// Deploy mocks when we are on local anvil chain!
+// keep track of contract addresses across diffrent chains
+// Sepolia ETH/USD -> diffrent address
+// Mainnet ETH/USD -> diffrent address
+
+pragma solidity ^0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    // If we are on a local anvil we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD -> priceFeed Address
+    }
+
+    // constructor
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+    }
+
+    // prlm
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+    }
+}
