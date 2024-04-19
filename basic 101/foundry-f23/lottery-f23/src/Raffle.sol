2bff2669980d601e0da23cb4ebe89d7914044b05
panditdhamdhere <dhamdherepandit@gmail.com>
2024-04-19 21:21:50 +0530

    vrf

--- basic 101/foundry-f23/lottery-f23/src/Raffle.sol
+++ basic 101/foundry-f23/lottery-f23/src/Raffle.sol
@@ -1,5 +1,28 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 
 /**
  * @title -> A Sample Raffle smart Contract
@@ -8,15 +31,74 @@ pragma solidity ^0.8.19;
  *@dev -> Implements Chainlink VRFv2
  */
 contract Raffle {
-    uint256 private immutable i_entranceFee;
+    // coustom errors
+    error Raffle__NotEnoughEthSent();
 
-    constructor(uint256 entranceFee) {
+    // constant variables
+    uint256 private constant REQUEST_CONFIRMATION = 3;
+    uint256 private constant NUM_WORDS = 1;
+
+    // state variables
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    // events
+    // whenever we do storage update we should emit an event
+    // 1. events make migration easy
+    // 2. frontend indexing easier !
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Insufficient Amount");
 
-    function pickWinner() public {}
+        // revert with custom errors are more gas efficiant than require with conditions;
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    // 1. get a random number
+    // 2. Use the random number to pick the winner
+    // 3. Be automatocally called
+    function pickWinner() external {
+        // check to see if enough time has passed
+
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
 
     /**Getter Functions */
 
